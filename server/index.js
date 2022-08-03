5fb0ead867c9b155dcf9fd2c31e6875aab6d46b8
bombin78 <bombin78@mail.ru>
2022-08-03 22:02:30 +0300

    db connect

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+    try {
+        // Вызов функции для подключения к БД
+        await sequelize.authenticate()
+        // Функция сверки состояния БД со схемой данных
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
