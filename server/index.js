617cd75f5c6b867aab99d5270244b69efa94f014
bombin78 <bombin78@mail.ru>
2022-08-09 21:56:28 +0300

    creating the model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,9 +10,9 @@ const app = express();
 const start = async () => {
     try {
         // Вызов функции для подключения к БД
-        await sequelize.authenticate()
+        await sequelize.authenticate();
         // Функция сверки состояния БД со схемой данных
-        await sequelize.sync()
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e);
