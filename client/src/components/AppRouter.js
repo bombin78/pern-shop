a0f6f5e1cc7d37ebe208dc7320fad5ba30e4a185
bombin78 <bombin78@mail.ru>
2022-08-27 17:07:22 +0300

    роутинг

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,61 @@
+import React 					from 'react';
+import { useRoutes } 			from 'react-router-dom';
+import { Navigate } 			from 'react-router-dom';
+import { ADMIN_ROUTE } 			from "../utils/consts";
+import { BASKET_ROUTE } 		from "../utils/consts";
+import { LOGIN_ROUTE } 			from "../utils/consts";
+import { REGISTRATION_ROUTE } 	from "../utils/consts";
+import { SHOP_ROUTE } 			from "../utils/consts";
+import { DEVICE_ROUTE } 		from "../utils/consts";
+import Admin 					from "../pages/Admin";
+import Basket 					from "../pages/Basket";
+import Auth 					from "../pages/Auth";
+import Shop						from "../pages/Shop";
+import DevicePage 				from "../pages/DevicePage";
+
+
+const AppRouter = () => {
+	const isAuth = false;
+
+	const authRouters = useRoutes([
+		{
+			path: ADMIN_ROUTE,
+			element: <Admin />,
+		},
+		{
+			path: BASKET_ROUTE,
+			element: <Basket />,
+		},
+	]);
+
+	const publicRouters = useRoutes([
+		{
+			path: LOGIN_ROUTE,
+			element: <Auth />,
+		},	{
+			path: REGISTRATION_ROUTE,
+			element: <Auth />,
+		},
+		{
+			path: SHOP_ROUTE,
+			element: <Shop />,
+		},
+		{
+			path: DEVICE_ROUTE + '/:id',
+			element: <DevicePage />,
+		},
+		{
+			path: '/*',
+			element: <Navigate replace to={ SHOP_ROUTE } />,
+		},
+	]);
+
+	return (
+		<>
+			{ isAuth && authRouters }
+			{ publicRouters }
+		</>
+	);
+};
+
+export default AppRouter;
